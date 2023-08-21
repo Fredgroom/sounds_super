2ff16934b8b363eb30d1a6c9df31bb619762e579
Freddie Groom <fredgroom@gmail.com>
2023-08-21 16:30:02 +1000

    update see what happens when I try to host dist on github

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), viteSingleFile()],
+});
