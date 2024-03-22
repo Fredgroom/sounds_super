4a2f34e12d6e0ed2a943076767a46284fcaceada
Freddie Groom <fredgroom@gmail.com>
2024-03-22 23:16:11 +1100

    update video

--- src/components/sections/video.tsx
+++ src/components/sections/video.tsx
@@ -11,11 +11,7 @@ const Video = () => {
       fullHeight
       // className='min-h-screen flex flex-col justify-center items-center w-full'
     >
-      {/* <SectionContainer
-        center
-        // className='w-full max-w-6xl flex justify-center'
-      > */}
-      <video
+      {/* <video
         muted
         preload='auto'
         autoPlay
@@ -24,7 +20,35 @@ const Video = () => {
         className='aspect-square w-[100vw] md:pt-[94px] lg:max-w-[60vw]  max-h-[85vw]'
       >
         <source src='/reel.mp4' type='video/mp4' />
-      </video>
+      </video> */}
+
+      <iframe
+        // src='https://player.vimeo.com/video/926228437?h=4e68b524b0&autoplay=1&loop=1&title=0&byline=0&portrait=0'
+        src='https://player.vimeo.com/video/926228437?background=1'
+        className='aspect-square w-full max-h-[75vh] pt-[56px] lg:pt-[94px]'
+        allow='autoplay; background'
+      ></iframe>
+      <>
+        {/* <video
+          className='rellax'
+          id='hero-video'
+          autoplay=''
+          loop={true}
+          muted={true}
+          preload='auto'
+          playsinline=''
+          data-desktop-src='https://player.vimeo.com/progressive_redirect/playback/812363685/rendition/1080p/file.mp4?loc=external&amp;oauth2_token_id=1616966395&amp;signature=9efae13b23656b04017f2c55160905a96ebd25ea64252569c10aad9d27e1985d'
+          data-mobile-src='https://player.vimeo.com/progressive_redirect/playback/812364317/rendition/720p/file.mp4?loc=external&amp;oauth2_token_id=1616966395&amp;signature=3c6536926eb64d041adf405a28ec15b388bbcfd98c8f24d911056dde8b84623a'
+          style={{ transform: 'translate3d(0px, 0px, 0px)' }}
+        >
+          <source
+            src='https://player.vimeo.com/progressive_redirect/playback/812363685/rendition/1080p/file.mp4?loc=external&amp;oauth2_token_id=1616966395&amp;signature=9efae13b23656b04017f2c55160905a96ebd25ea64252569c10aad9d27e1985d'
+            type='video/mp4'
+          />
+        </video> */}
+      </>
+      {/* <script src='https://player.vimeo.com/api/player.js'></script> */}
+      {/* <script src='https://player.vimeo.com/api/player.js'></script> */}
       {/* <video width="320" height="240" controls>
   <source src="movie.mp4" type="video/mp4">
   <source src="movie.ogg" type="video/ogg">
@@ -32,7 +56,6 @@ Your browser does not support the video tag.
 </video> */}
       {/* <img src={gif} className='min-w-[500px]' /> */}
       {/* <Logo size={600} /> */}
-      {/* </SectionContainer> */}
     </Section>
   );
 };
