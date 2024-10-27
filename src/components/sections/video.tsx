12a66ba712397660f2fb91d7cff08b6d550efa45
Freddie Groom <fredgroom@gmail.com>
2024-10-27 20:43:59 +0000

    update video tag for lazy loading

--- src/components/sections/video.tsx
+++ src/components/sections/video.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef } from 'react';
+// import Logo from '../Logo';
 
 const Video = () => {
   let videoRef = useRef<HTMLVideoElement>(null);
@@ -8,20 +9,24 @@ const Video = () => {
     }
   }, []);
   return (
-    <div className='w-full h-screen max-w-full flex flex-col items-center justify-center pt-[76px] md:pt-[96px]'>
-      <video
-        ref={videoRef}
-        controls={false}
-        autoPlay
-        playsInline
-        muted
-        loop
-        className='max-h-full'
-      >
-        <source src='./reel.mp4' type='video/mp4' />
-        Your browser does not support the video tag.
-      </video>
-    </div>
+    <>
+      {/* {!videoRef.current && <Logo />} */}
+      <div className='w-full h-screen max-w-[900px] flex flex-col items-center justify-center md:pt-[96px]'>
+        <video
+          ref={videoRef}
+          controls={false}
+          autoPlay
+          playsInline
+          muted
+          loop
+          className='max-h-full'
+          poster='sounds_super.jpg'
+        >
+          <source src='./reel.mp4' type='video/mp4' />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+    </>
   );
 };
 
